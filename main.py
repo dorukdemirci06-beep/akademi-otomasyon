4fcf947b7145842e3b2c3eedaf09a18b5a2f5314
Doruk Demirci <dorukdemirci06@gmail.com>
2026-09-15 15:21:49 +0300

    fix(cors): allow network IP access for other devices

--- main.py
+++ main.py
@@ -158,6 +158,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
